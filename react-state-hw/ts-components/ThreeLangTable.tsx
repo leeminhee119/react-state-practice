425db77fc0d1013ef69b4adf3ad902a076efe150
이민희 <mhl98@dgu.ac.kr>
2022-11-23 01:05:11 +0900

    base

---
+++ react-state-hw/ts-components/ThreeLangTable.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const ThreeLangTable = () => {
+    return (
+        <table>
+            <thead></thead>
+            <tbody></tbody>
+        </table>
+    )
+}
+export default ThreeLangTable;
